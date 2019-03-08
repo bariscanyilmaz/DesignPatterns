fd2492e806d620136d78c3eb49054a37bac899d9
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2019-03-08 23:22:41 +0300

    facade,prototype,builder added

---
+++ Facade/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    interface ILogging
+    {
+        void Log();
+    }
+
+    class Logging:ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+
+    interface ICaching
+    {
+        void Cache();
+    }
+
+    class Caching:ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Caching");
+        }
+    }
+
+    interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    class Authorize:IAuthorize
+
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("User Checked");
+        }
+    }
+
+    class CustomerManager
+    {
+        private CrossCuttingConcernsFacade concernsFacade;
+
+        public CustomerManager()
+        {
+            concernsFacade = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            concernsFacade.Authorize.CheckUser();
+            concernsFacade.Caching.Cache();
+            concernsFacade.Logging.Log();
+            concernsFacade.Validation.Validate();
+            Console.WriteLine("Saved");
+        }
+        
+
+    }
+
+    interface IValidate
+    {
+        void Validate();
+    }
+
+    class Validetation : IValidate
+    {
+        public void Validate()
+        {
+            Console.WriteLine("Validated");
+        }
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+        public IValidate Validation;
+        public CrossCuttingConcernsFacade()
+        {
+            Validation = new Validetation();
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { FirtsName="Engin",LastName="Demiroğ",City="Ankara",Id=1};
+            
+
+            Customer customer2 =(Customer) customer.Clone();
+            customer2.FirtsName = "Salih";
+            Console.WriteLine(customer.FirtsName);
+            Console.WriteLine(customer2.FirtsName);
+
+
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+
+        public int Id { get; set; }
+        public string FirtsName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();    
+        }
+    }
+
+    public class Employee: Person
+    {
+        public decimal Salary{ get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+}
