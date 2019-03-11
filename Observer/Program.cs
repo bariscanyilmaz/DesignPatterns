8294f2769670059b588e456e775b576f07438622
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2019-03-11 21:18:01 +0300

    Strategy and Observer

---
+++ Observer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager();
+            productManager.Attach(new CustomerObserver());
+            productManager.Attach(new EmployeeObserver());
+            productManager.UpdatePrice();
+        }
+    }
+
+    class ProductManager
+    {
+        List<Observer> observers = new List<Observer>();
+
+        public void UpdatePrice()
+        {
+            Console.WriteLine("Product Price Updated");
+            Notify();
+        }
+
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+        private void Notify()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message To Customer : Product Price Change");
+        }
+    }
+
+    class EmployeeObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new After2010CreditCalculater();
+            customerManager.SaveCredit();
+
+
+            customerManager.CreditCalculatorBase = new Before2010CreditCalculater();
+            customerManager.SaveCredit();
+            Console.ReadLine();
+            
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculater : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using Before2010");
+        }
+    }
+
+
+    class After2010CreditCalculater : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using After2010");
+        }
+    }
+
+    class CustomerManager
+    {
+
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer Manager Business");
+            CreditCalculatorBase.Calculate();
+
+        }
+    }
+}
