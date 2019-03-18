52fd73fc18045bbd611f6a07f6055d402d5f8809
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2019-03-18 19:32:36 +0300

    mediator and command patterns added

---
+++ Command/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockManager stockManager = new StockManager();
+            BuyStock buyStock = new BuyStock(stockManager);
+            SellStock sellStock = new SellStock(stockManager);
+
+            StockController stockController = new StockController();
+            stockController.TakeOrder(buyStock);
+            stockController.TakeOrder(sellStock);
+            stockController.TakeOrder(buyStock);
+            stockController.TakeOrder(sellStock);
+
+            stockController.PlaceOrders();
+            Console.ReadLine();
+
+
+        }
+
+    }
+
+    class StockManager
+    {
+        private string name = "Laptop";
+        private int quantity = 10;
+        
+        public void Buy()
+        {
+            Console.WriteLine("Stock :{0} ,{1} bought!!",name,quantity);
+        }
+
+        public void Sell()
+        {
+            Console.WriteLine("Stock :{0} ,{1} sold!!", name, quantity);
+
+        }
+    }
+
+    interface IOrder
+    {        
+        void Execute();
+    }
+
+    class BuyStock : IOrder
+    {
+        private StockManager stockManager;
+        public BuyStock(StockManager _stockManager)
+        {
+            stockManager = _stockManager;
+        }
+
+        public void Execute()
+        {
+            stockManager.Buy();
+        }
+    }
+
+    class SellStock : IOrder
+    {
+        private StockManager stockManager;
+        public SellStock(StockManager _stockManager)
+        {
+            stockManager = _stockManager;
+        }
+        public void Execute()
+        {
+            stockManager.Sell();
+        }
+    }
+
+    class StockController
+    {
+        List<IOrder> orders = new List<IOrder>();
+        public void TakeOrder(IOrder order)
+        {
+            orders.Add(order);
+        }
+
+        public void PlaceOrders()
+        {
+            foreach (var order in orders)
+            {
+                order.Execute();
+            }
+
+            orders.Clear();
+        }
+
+    }
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+            Teacher engin = new Teacher(mediator) {Name="Engin" };
+
+            mediator.Teacher = engin;
+
+            Student derin = new Student(mediator) { Name="Derin"};
+            Student baris = new Student(mediator) { Name = "Barış" };
+            mediator.Students = new List<Student>() { derin,baris};
+
+            engin.SendNewImageUrl("slide1.jpg");
+            engin.RecieveQuestion("is it true", baris);
+
+        }
+    }
+
+    abstract class CourseMember
+    {
+        protected Mediator Mediator;
+        public CourseMember(Mediator _mediator)
+        {
+            Mediator = _mediator;
+        }
+    }
+
+    class Teacher : CourseMember
+    {
+        
+
+        public Teacher(Mediator _mediator) : base(_mediator)
+        {
+        }
+        public string Name { get; set; }
+        public void RecieveQuestion(string question, Student student)
+        {
+            Console.WriteLine("Teacher recieved a quesiton from {0} ,{1}",student.Name,question);
+        }
+
+        public void SendNewImageUrl(string url)
+        {
+            Console.WriteLine("Teacher changed slide:{0} ",url);
+            Mediator.UpdateImage(url);
+        }
+
+        public void AnswerQuestion(string answer,Student student)
+        {
+            Console.WriteLine("Teacher answered quesiton {0} {1}",answer,student.Name);
+            Mediator.SendAnswer(answer,student);
+        }
+
+    }
+
+    class Student : CourseMember
+    {
+        public Student(Mediator _mediator) : base(_mediator)
+        {
+        }
+
+        public string Name { get; set; }
+
+        public void RecieveIamge(string url)
+        {
+            Console.WriteLine("{1} Recieved Image: {0}",url,Name);
+        }
+
+        public void RecieveAnswer(string answer)
+        {
+            Console.WriteLine("{1} recieved answer {0}",answer,Name);
+        }
+    }
+
+    class Mediator
+    {
+        public Teacher Teacher { get; set; }
+        public List<Student> Students { get; set; }
+
+        public void UpdateImage(string url)
+        {
+            foreach (var student in Students)
+            {
+                student.RecieveIamge(url);
+            }
+        }
+
+        public void SendQuestion(string question,Student student)
+        {
+            Teacher.RecieveQuestion(question, student);
+        }
+
+        public void SendAnswer(string answer,Student student)
+        {
+            student.RecieveAnswer(answer);
+        }
+    
+    }
+
+
+}
